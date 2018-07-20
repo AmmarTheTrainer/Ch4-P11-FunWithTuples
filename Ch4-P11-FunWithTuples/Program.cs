5738c90cd7764d0c60de106d4b04b14006f7493c
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-07-20 17:19:36 +0500

    first commit from lec

---
+++ Ch4-P11-FunWithTuples/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch4_P11_FunWithTuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" lecture 4 - Project 11 - Tuples ");
+
+            Console.ReadLine();
+        }
+    }
+}
